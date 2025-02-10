9e0828e905a4c41a1986b80fd64fc34377e6741e
Sabari-Ramesh <sabariramesh2002@gmail.com>
2025-02-10 10:49:22 +0530

    Refactor : Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,6 @@ import React from "react";
 function Header({ year, month, handlePrevMonth, handleNextMonth }) {
   return (
     <div className="flex relative max-w-[78rem] mx-auto rounded bg-blue-500">
-      <button className="px-2 py-1 ml-8 mt-1 mb-1 text-white rounded-xl text-lg font-base border border-gray-700">
-        Today
-      </button>
-
       <button
         className="px-4 py-2 text-white rounded font-bold text-2xl"
         onClick={handlePrevMonth}
